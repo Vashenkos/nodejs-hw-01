3682fe441ebf5619ac521156aa078e47c8c982ad
Vashenkos <159846523+Vashenkos@users.noreply.github.com>
2024-11-11 21:02:58 +0200

    111

--- src/utils/readContacts.js
+++ src/utils/readContacts.js
@@ -1,3 +1,15 @@
+import fs from 'fs/promises';
 import { PATH_DB } from '../constants/contacts.js';
 
-export const readContacts = async () => {};
+
+const readContacts = async () => {
+    try {
+        const data = await fs.readFile(PATH_DB, 'utf-8');
+        return JSON.parse(data);
+    } catch (error) {
+        console.error('Error contacts:', error);
+        throw error;
+    }
+};
+
+export { readContacts };
